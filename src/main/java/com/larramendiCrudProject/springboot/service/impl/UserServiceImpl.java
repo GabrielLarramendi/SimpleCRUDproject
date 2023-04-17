456cd6d725944075620e97e174bfdac530e8bf40
Larramendi <larramendiga@gmail.com>
2023-04-17 11:36:06 -0300

    Using ModelMapper Library to Map Entity to DTO and Vice Versa: Refactored code. Changing UserMapper to ModelMapper library

--- src/main/java/com/larramendiCrudProject/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/larramendiCrudProject/springboot/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.larramendiCrudProject.springboot.mapper.UserMapper;
 import com.larramendiCrudProject.springboot.repository.UserRepository;
 import com.larramendiCrudProject.springboot.service.UserService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,34 +19,44 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
-        User user = UserMapper.mapToUser(userDto);
+
+        //User user = UserMapper.mapToUser(userDto);
+        User user = modelMapper.map(userDto, User.class);
         User savedUser = userRepository.save(user);
-        UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+
+        //UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+        UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
         return savedUserDto;
     }
 
     @Override
     public UserDto getUserById(Long userId) {
         Optional<User> optionalUser = userRepository.findById(userId);
-        UserDto userDto = UserMapper.mapToUserDto(optionalUser.get());
+
+        //UserDto userDto = UserMapper.mapToUserDto(optionalUser.get());
+        UserDto userDto = modelMapper.map(optionalUser.get(), UserDto.class);
         return userDto;
     }
 
     @Override
     public List<UserDto> getAllUsers() {
         List<User> list = userRepository.findAll();
-        List<UserDto> listDto = new ArrayList<>();
 
-        /*for(User x : list) {
-            listDto.add(UserMapper.mapToUserDto(x));
-        }
-        return listDto; */
+//        for(User x : list) {
+//            listDto.add(UserMapper.mapToUserDto(x));
+//        }
+//        return listDto;
 
-        return list.stream().map(UserMapper::mapToUserDto)
+//        return list.stream().map(UserMapper::mapToUserDto)
+//                .collect(Collectors.toList()); //Converte a lista de User para uma lista de UserDto
+
+        return list.stream().map((user) -> modelMapper.map(user, UserDto.class))
                 .collect(Collectors.toList()); //Converte a lista de User para uma lista de UserDto
+
     }
 
     @Override
@@ -56,7 +67,8 @@ public class UserServiceImpl implements UserService {
         existentUser.setEmail(userDto.getEmail());
 
         User updatedUser = userRepository.save(existentUser);
-        return UserMapper.mapToUserDto(updatedUser);
+        //return UserMapper.mapToUserDto(updatedUser);
+        return modelMapper.map(updatedUser, UserDto.class);
     }
 
     @Override
