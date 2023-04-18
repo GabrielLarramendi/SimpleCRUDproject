a3ab50607ad49c0ab4a1bf3c5487d1bea8f33274
Larramendi <larramendiga@gmail.com>
2023-04-17 23:57:07 -0300

    Adding MapStruct Library maven dependencies and creating Mapper Class: AutoUserMapper'

---
+++ src/main/java/com/larramendiCrudProject/springboot/mapper/AutoUserMapper.java
@@ -0,0 +1,17 @@
+package com.larramendiCrudProject.springboot.mapper;
+
+import com.larramendiCrudProject.springboot.dto.UserDto;
+import com.larramendiCrudProject.springboot.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoUserMapper {
+
+    AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class); //Pode usar o MAPPER parachamar um desses dois metodos
+
+    UserDto mapToUserDto(User user);
+    User mapToUser(UserDto userDto);
+
+}
